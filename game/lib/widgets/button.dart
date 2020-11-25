bd2d8324371eb10a13bc87fce126a84277d56c9d
Erick Zanardo <erickzanardoo@gmail.com>
2020-11-25 18:58:58 -0300

    Adding a state to the button

--- game/lib/widgets/button.dart
+++ game/lib/widgets/button.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:life_on_moon/palette.dart';
 import 'package:life_on_moon/widgets/label.dart';
 
-class GameButton extends StatelessWidget {
+class GameButton extends HookWidget {
   final String label;
   final VoidCallback onPress;
 
@@ -10,11 +11,19 @@ class GameButton extends StatelessWidget {
 
   @override
   Widget build(_) {
+    final pressed = useState(false);
+
     return Listener(
-        onPointerUp: (_) => onPress(),
+        onPointerDown: (_) {
+          pressed.value = true;
+        },
+        onPointerUp: (_) {
+          pressed.value = false;
+          onPress();
+        },
         child: Container(
             height: 40,
-            color: Palette.PASTEL_BLUE,
+            color: pressed.value ? Palette.PASTEL_DARK_BLUE : Palette.PASTEL_BLUE,
             child: Center(child: GameLabel(label)),
         ),
     );
